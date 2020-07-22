874ab7600bb53115675c7958b512295cc850796b
AmolShingane <68585306+AmolShingane@users.noreply.github.com>
2020-07-22 09:24:20 +0530

    Add files via upload

---
+++ Day_2_Assignment.py.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello word")
+
+
+# In[18]:
+
+
+name="Amol"
+marks=90
+age=25
+print("Name of student is %s\tand marks is %d\t and age is %d\t"%(name,marks,age))
+
+
+# In[7]:
+
+
+a=20
+b=20
+id(a)
+id(b)
+
+
+# In[8]:
+
+
+5**4
+
+
+# In[12]:
+
+
+print('it\'s amol')
+
+
+# In[13]:
+
+
+print("Amol\nShingane")
+
+
+# In[19]:
+
+
+name="Amol"
+marks=90
+age=25
+print(f"Name of student is {name}\tand marks is {marks}\t and age is %d\t{age}")
+
+
+# In[20]:
+
+
+10//3
+
+
+# In[22]:
+
+
+10>9 and 8>7
+
+
+# In[23]:
+
+
+10>9 and 8>9
+
+
+# In[24]:
+
+
+10>9 or 8>9
+
+
+# In[ ]:
+
+
+
+
